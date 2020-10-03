68799ad18146e791f8cea378985251c4c88695e2
Eren Doru <erendoru@hotmail.com>
2020-10-03 16:41:55 +0300

    easy sass landing page structure
    a landing page structure for reusable data based components

---
+++ src/components/Button.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Button.css";
+
+const STYLES = ["btn-outline", "btn-primary"];
+
+const SIZED = ["btn--medium", "btn--large", "btn--mobile", "btn--wide"];
+
+const COLOR = ["primary", "blue", "red", "green"];
+
+export const Button = ({
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize,
+  buttonColor,
+}) => {
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
+
+  const checkButtonSize = SIZED.includes(buttonSize) ? buttonSize : SIZED[0];
+
+  const checkButtonColor = COLOR.includes(buttonColor) ? buttonColor : null;
+  return (
+    <button
+      className={`btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonColor}`}
+      onClick={onClick}
+      type={type}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
